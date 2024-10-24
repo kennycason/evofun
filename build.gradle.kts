6d374fad153238a664b5d4cc74385cf7876a98b8
Kenny Cason <kenneth.cason@gmail.com>
2024-10-24 00:17:36 -0700

    beat detection/response + read from dna farm

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,10 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "TarsosDSP repository"
+        url = uri("https://mvn.0110.be/releases")
+    }
 }
 
 dependencies {
@@ -14,6 +18,9 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
+//    implementation("be.tarsos.dsp:core:2.5")
+//    implementation("be.tarsos.dsp:jvm:2.5")
+    implementation("com.github.wendykierp:JTransforms:3.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
--- src/main/kotlin/evofun/bio/DNAFarm.kt
+++ src/main/kotlin/evofun/bio/DNAFarm.kt
@@ -2,6 +2,7 @@ package evofun.bio
 
 import evofun.bio.serialize.DNASerializer
 import java.io.File
+import kotlin.random.Random
 
 class DNAFarm(
     private val rooLocation: File = File(System.getProperty("user.home"), ".evofun")
@@ -19,6 +20,26 @@ class DNAFarm(
         dnaFile.writeText(dnaJsonString)
         println("DNA saved to $dnaFile")
     }
+
+    fun readRandomDNA(): DNA? {
+        // List all JSON files in the dnaLocation directory
+        val dnaFiles = dnaLocation.listFiles { file -> file.extension == "json" } ?: return null
+
+        // Ensure there are files to read from
+        if (dnaFiles.isEmpty()) {
+            println("No DNA files found in the directory.")
+            return null
+        }
+
+        // Select a random file from the list
+        val randomFile = dnaFiles[Random.nextInt(dnaFiles.size)]
+        println("Reading DNA from file: ${randomFile.name}")
+
+        // Read the file content and deserialize it into a DNA object
+        val dnaJsonString = randomFile.readText()
+        return DNASerializer.deserialize(dnaJsonString)
+    }
+
 
     private fun initIfNeeded() {
         if (!rooLocation.exists()) {
